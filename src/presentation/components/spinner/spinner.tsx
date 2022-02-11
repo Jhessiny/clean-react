f36ac97cc8bb95495ede6105d4586efb1a7878c7
Jhessiny <jhessinymattos@gmail.com>
2022-02-11 12:58:53 -0300

    feat: spinner

---
+++ src/presentation/components/spinner/spinner.tsx
@@ -0,0 +1,19 @@
+
+import styles from './spinner-styles.scss'
+
+import React from 'react'
+
+type Props = React.HTMLAttributes<HTMLElement> & {
+  isNegative?: boolean
+}
+
+const Spinner: React.FC<Props> = ({ isNegative, ...props }: Props) => {
+  const negativeClass = isNegative ? styles.negative : ''
+  return (
+    <div {...props} data-testid='spinner' className={[styles.spinner, negativeClass, props.className].join(' ')}>
+      <div /><div /><div /><div />
+    </div>
+  )
+}
+
+export default Spinner
