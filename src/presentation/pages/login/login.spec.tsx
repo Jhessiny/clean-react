5451f5b0c5c671df3015856e14fbb9e988ada634
Jhessiny <jhessinymattos@gmail.com>
2022-02-13 20:46:24 -0300

    test: ensure login inputs start with correct initial state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -5,21 +5,34 @@ import { render, screen } from '@testing-library/react'
 import Login from './login'
 
 describe('Initial login state', () => {
-  it('should not render spinner', () => {
+  beforeEach(() => {
     render(<Login/>)
+  })
+
+  it('should not render spinner', () => {
     const spinner = screen.queryByTestId('spinner')
     expect(spinner).toBeNull()
   })
 
   it('should not render form-error', () => {
-    render(<Login/>)
     const formStatus = screen.queryByTestId('form-error')
     expect(formStatus).toBeNull()
   })
 
   it('should have submit button disabled', () => {
-    render(<Login/>)
     const submitButton = screen.getByRole('button', { name: /entrar/i }) as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
+  })
+
+  it('should have email status with correct title', () => {
+    const emailInput = screen.getByPlaceholderText(/Digite seu email/i)
+    const emailStatus = emailInput.nextSibling as HTMLElement
+    expect(emailStatus.title).toBe('Campo obrigatório')
+  })
+
+  it('should have password status with correct title', () => {
+    const passwordInput = screen.getByPlaceholderText(/Digite sua senha/i)
+    const passwordStatus = passwordInput.nextSibling as HTMLElement
+    expect(passwordStatus.title).toBe('Campo obrigatório')
   })
 })
